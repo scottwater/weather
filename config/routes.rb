5475e7c7fba28f5fc4ece297fc53d14209a4c897
Scott Watermasysk <scottwater@gmail.com>
2024-08-23 15:58:23 -0400

    Add weather route

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
+  get "/weather" => "home#show", as: :weather
   root "home#index"
 end
--- test/components/search_box_component_test.rb
+++ test/components/search_box_component_test.rb
@@ -3,8 +3,11 @@
 require "test_helper"
 
 class SearchBoxComponentTest < ViewComponent::TestCase
+  include Rails.application.routes.url_helpers
   test "renders the seeach box" do
     render_inline(SearchBoxComponent.new)
-    assert_selector("form")
+    assert_selector("input[name='address']")
+    assert_selector("form[action='#{weather_path}']")
   end
+
 end
