35e6f0f581f1d8edc6a9e12e006c1b48e96ae62f
Scott Watermasysk <scottwater@gmail.com>
2024-08-23 13:33:57 -0400

    Optionally disable browser restrictions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Allow modern browsers and Sizzy app
+  allow_browser versions: :modern,
+                unless: -> { ENV["SKIP_BROWSER_CHECK"] == "true" }
 end
