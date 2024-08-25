8f9ab0dd9777cfb47bc25d3fe7a45301ce419635
Scott Watermasysk <scottwater@gmail.com>
2024-08-24 22:55:19 -0400

    Extend IpClient with a lookup method to find a specific ip

--- app/clients/ip_client.rb
+++ app/clients/ip_client.rb
@@ -18,4 +18,11 @@ class IpClient
     # and related location info
     IpResponse.new(self.get("/json"))
   end
+
+  def self.lookup(ip_address)
+    response = self.get("/json/#{ip_address}")
+    if response["status"] == "success"
+      IpResponse.new(response)
+    end
+  end
 end
--- test/clients/ip_client_test.rb
+++ test/clients/ip_client_test.rb
@@ -7,4 +7,12 @@ class IpClientTest < ActiveSupport::TestCase
       assert_equal "Warren Township New Jersey, United States", response.location
     end
   end
+
+  test "IpClient#lookup" do
+    VCR.use_cassette("lookup") do
+      response = IpClient.lookup("72.68.111.45")
+      assert_equal "72.68.111.45", response.ip
+      assert_equal "Warren Township New Jersey, United States", response.location
+    end
+  end
 end
