6dcf294fd99b627c1bda8c1b94a5fc502e2f8327
Scott Watermasysk <scottwater@gmail.com>
2024-08-26 14:23:10 -0400

    allow services/clients to be injected into WeatherService

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -3,8 +3,15 @@
 class WeatherService
   attr_reader :address
 
-  def initialize(address)
+  def initialize(address,
+    coordinates_service: CoordinatesService,
+    grid_point_service: GridPointService,
+    weather_client: WeatherClient)
+
     @address = address
+    @coordinates_service = coordinates_service
+    @grid_point_service = grid_point_service
+    @weather_client = weather_client
   end
 
   def call
@@ -16,7 +23,7 @@ class WeatherService
   def coordinates
     if !defined?(@coordinates)
       Rails.logger.info "Looking up weather for coordinates for #{address}"
-      @coordinates = CoordinatesService.new(address).call
+      @coordinates = @coordinates_service.new(address).call
     end
     @coordinates
   end
@@ -24,7 +31,7 @@ class WeatherService
   def grid_point
     if coordinates.valid? && !defined?(@grid_point)
       Rails.logger.info "Looking up weather for grid point #{coordinates.lat}, #{coordinates.lon}"
-      @grid_point = GridPointService.new(coordinates).call
+      @grid_point = @grid_point_service.new(coordinates).call
     end
 
     @grid_point
@@ -33,7 +40,7 @@ class WeatherService
   def periods
     if grid_point && !defined?(@periods)
       Rails.logger.info "Looking up weather for periods for grid point #{grid_point.grid_id}, #{grid_point.grid_x}, #{grid_point.grid_y}"
-      @periods = WeatherClient.periods_by_grid_point(grid_point)
+      @periods = @weather_client.periods_by_grid_point(grid_point)
     end
 
     @periods
